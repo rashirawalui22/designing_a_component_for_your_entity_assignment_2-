09f3391a4ac41341c5a567d4291ffb7586ae5843
rashirawal22 <rashi.rawal@kalvium.community>
2025-02-18 10:47:37 +0530

    Completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,8 @@
-// write button card here
+import React from "react";
+import "./Button.css"; // Import button styling
+
+const ViewProductButton = () => {
+  return <button className="view-btn">View Product</button>;
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,21 @@
 // write product card here
+import React from "react";
+import ViewProductButton from "../components/button"; 
+import "./ProductCard.css"; 
+
+const Productcard = () => {
+  const productImage = "https://cms.dresma.com/uploads/Image_2_11cac6fa82.jpg"; 
+  const productName = "Perfume";
+  const productPrice = "$4998.99";
+
+  return (
+    <div className="product-card">
+      <img src={productImage} alt="Product" />
+      <h2>{productName}</h2>
+      <p className="price">{productPrice}</p>
+      <ViewProductButton /> {}
+    </div>
+  );
+};
+
+export default Productcard;
